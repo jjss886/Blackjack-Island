084d3ec0ad14f22cc3887e4e118df4a105e7e445
James Shen <jjss886@gmail.com>
2020-02-29 00:53:18 -0500

    resetting with new project

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Blackjack
+
+Still in progress (adding in moving "Staying" button and Ace functionality)...please come back soon!
+
+**Tech Stack**: React, Redux
+
+**Deployed**: https://blackjack-island-react.herokuapp.com/
---
+++ src/components/Tracker.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Tracker extends Component {
+  render() {
+    const { players } = this.props;
+
+    return (
+      <div className="trackerFullDiv">
+        <h3>Money Tracker!</h3>
+
+        <hr className="trackerHR" />
+
+        {players.length ? (
+          players.map((player, idx) => (
+            <span key={idx} className="trackPlayer">
+              <strong>{player.Name}</strong>: {!isNaN(player.Cash) ? "$" : null}
+              {player.Cash}
+            </span>
+          ))
+        ) : (
+          <span className="trackPlayer">No Players</span>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    players: state.players
+  };
+};
+
+export default connect(mapState)(Tracker);
